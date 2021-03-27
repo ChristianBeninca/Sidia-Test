bb1127157eaa5d267a2f6ade60bee5f6f89712cd
ChristianBeninca <christianbeninca@outlook.com>
2021-03-27 10:42:26 -0300

    First Backup: Menu
    •Start
    •Quit
    •Credits
    •Animation
    •Options (only button)

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager instance { get { return instance_; } }
+    private static MenuManager instance_;
+
+    void Awake()
+    {
+        instance_ = this;
+    }
+
+    public void ButtonInteraction(string a)
+    {
+        switch (a)
+        {
+            case "Start":
+                SceneManager.LoadScene(1);
+                break;
+            case "Quit":
+                Application.Quit();
+                break;
+            case "Options":
+                Debug.LogWarning("Menu de opções em desenvolvimento");
+                break;
+            case "Reserva":
+                break;
+            default:
+                Debug.LogWarning("Função não atribuida ao botão");
+                break;
+        }
+    }
+}
